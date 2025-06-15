4d85542fce7c5f6a44dc46171081c3022e969853
Norsion <infaer.res@yandex.ru>
2025-06-15 20:34:11 +0400

    vscode: clear extension, add user settings

--- home/config/vscode/default.nix
+++ home/config/vscode/default.nix
@@ -17,5 +17,29 @@
       package = pkgs.vscodium;
       enableUpdateCheck = false;
       enableExtensionUpdateCheck = false;
+
+      userSettings = {
+        "chat.editor.fontFamily"= lib.mkDefault "JetBrainsMono";
+        "chat.editor.fontSize"= 16.0;
+        "debug.console.fontFamily"= lib.mkDefault "JetBrainsMono";
+        "debug.console.fontSize"= 16.0;
+        "editor.fontFamily"= lib.mkDefault "JetBrainsMono";
+        "editor.fontSize"= 16.0;
+        "editor.inlayHints.fontFamily"= lib.mkDefault "JetBrainsMono";
+        "editor.inlineSuggest.fontFamily"= lib.mkDefault "JetBrainsMono";
+        "editor.minimap.sectionHeaderFontSize"= 10.285714285714286;
+        "extensions.autoCheckUpdates"= false;
+        "markdown.preview.fontFamily"= lib.mkDefault "JetBrainsMono";
+        "markdown.preview.fontSize"= 16.0;
+        "scm.inputFontFamily"= lib.mkDefault "JetBrainsMono";
+        "scm.inputFontSize"= 14.857142857142858;
+        "screencastMode.fontSize"= 64.0;
+        "terminal.integrated.fontSize"= 16.0;
+        "update.mode"= "none";
+        "workbench.colorTheme"= "Stylix";
+        "workbench.activityBar.location"= "top";
+        "workbench.secondarySideBar.defaultVisibility"= "visible";
+        "outline.showVariables"= false;
+      };
     };
 }
--- home/config/vscode/extensions/default.nix
+++ home/config/vscode/extensions/default.nix
@@ -6,41 +6,17 @@
 {
   programs.vscode.extensions = with pkgs.vscode-extensions; [
     jnoortheen.nix-ide
-    hashicorp.hcl
-    redhat.ansible
-    vscjava.vscode-java-pack
-    visualstudioexptteam.vscodeintellicode
-    visualstudioexptteam.intellicode-api-usage-examples
-    ms-vscode.cpptools-extension-pack
     stylelint.vscode-stylelint
-    reloadedextensions.reloaded-cpp
-    mathiasfrohlich.kotlin
-    hediet.vscode-drawio
-    hars.cppsnippets
-    firsttris.vscode-jest-runner
-    cweijan.dbclient-jdbc
-    vscjava.vscode-java-debug
     ms-azuretools.vscode-docker
     dbaeumer.vscode-eslint
-    gitlab.gitlab-workflow
     eamodio.gitlens
     golang.go
-    hashicorp.terraform
     ms-python.isort
-    wholroyd.jinja
-    ms-toolsai.jupyter
-    ms-toolsai.vscode-jupyter-cell-tags
-    ms-toolsai.jupyter-keymap
-    ms-toolsai.jupyter-renderers
-    ms-toolsai.vscode-jupyter-slideshow
-    redhat.java
     ritwickdey.liveserver
-    vscjava.vscode-maven
+    k--kato.intellij-idea-keybindings
     bbenoist.nix
     esbenp.prettier-vscode
-    vscjava.vscode-java-dependency
     ms-python.python
-    vscjava.vscode-java-test
     gruntfuggly.todo-tree
     xadillax.viml
     redhat.vscode-yaml
